d91c379d03843c90838832cb00d923aa306215fd
robnnl <robinprinsen@gmail.com>
2025-03-24 14:19:56 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  transpilePackages: ['@heroicons/react']
+  swcMinify: true
 }
 
 module.exports = nextConfig 
